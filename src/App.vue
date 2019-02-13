3979a6c0ce872ef3828b4999313cd0fb8c3df4e6
Jamie Popkin <popkinj@littleearth.ca>
2019-02-12 16:18:37 -0800

    Vue code

---
+++ src/App.vue
@@ -0,0 +1,115 @@
+
+<template lang="pug">
+
+  #app
+    .parentx-static
+      vs-sidebar.sidebar-primaryx(
+        static-position=""
+        color="primary"
+        spacer=""
+        v-model="active"
+        hidden-background=true
+      )
+        .header-sidebar(slot="header")
+          h4 Environmental Assessment
+
+        vs-sidebar-item(index="1" icon="build")
+          router-link(to="/") Settings
+
+        vs-sidebar-item(index="2" icon="get_app")
+          router-link(to="/proponents") Proponents/Customers
+
+        //- Place holders
+        vs-sidebar-item(index="4" icon="get_app")
+          | &nbsp; Accounts Receivable
+        vs-sidebar-item(index="5" icon="get_app")
+          | &nbsp; Invoices
+        vs-sidebar-item(index="6" icon="get_app")
+          | &nbsp; Customer Accounts
+        vs-sidebar-item(index="7" icon="get_app")
+          | &nbsp; Account Sites
+        vs-sidebar-item(index="8" icon="get_app")
+          | &nbsp; Site Contacts
+        vs-sidebar-item(index="9" icon="get_app")
+          | &nbsp; Deposits
+        vs-sidebar-item(index="10" icon="get_app")
+          | &nbsp; Credit Memos
+        vs-sidebar-item(index="11" icon="get_app")
+          | &nbsp; Receipts 
+        vs-sidebar-item(index="12" icon="get_app")
+          | &nbsp; Adjustments 
+    .page
+      keep-alive
+        transition(name="fade", mode="out-in")
+          router-view
+
+</template>
+
+<script>
+  export default {
+    data:()=>({
+      active:true
+    })
+  }
+
+/*TODO: Logic for flagging button as active for which ever
+  page you're on */
+  
+</script>
+
+<style lang=stylus>
+  .page
+    padding 2rem
+    margin-left 16rem
+
+  .vs-sidebar
+    background #f4f4f4 !important
+    margin-right 1rem !imporant
+
+  .parentx-static
+    overflow hidden
+    position absolute
+    width 20rem
+    height 100%
+
+    .sidebar-primaryx
+      height 100%
+
+  .header-sidebarx
+    display flex
+    align-items center
+    justify-content center
+    flex-direction column
+    width 100%
+    h4
+      display flex
+      align-items center
+      justify-content center
+      width 100%
+    button
+      margin-left 10px
+
+  .footer-sidebar
+    display flex
+    align-items center
+    justify-content space-between
+    width 100%
+    button
+      border 0px solid rgba(0,0,0,0) !important
+      border-left 1px solid rgba(0,0,0,.07) !important
+      border-radius 0px !important
+  #app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    text-align center
+    color #2c3e50
+
+  #nav
+    padding 30px
+    a
+      font-weight bold
+      color #2c3e50
+      &.router-link-exact-active
+        color #42b983
+</style>
---
+++ src/views/Proponents.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  div.proponents
+    table
+      tr
+        th Fetch all Proponents
+        th
+          vs-button(
+            id="button-with-loading"
+            class="vs-con-loading__container"
+            @click="openLoadingContained"
+            type="filled"
+            ref="loadableButton"
+            vslor="primary")
+              | Load
+</template>
+
+<script>
+  export default {
+    methods: {
+      openLoadingContained(){
+        this.$vs.loading({
+          background: this.backgroundLoading,
+          color: this.colorLoading,
+          container: '#button-with-loading',
+          scale: 0.45
+        });
+        setTimeout( () => {
+          this.$vs.loading.close('#button-with-loading .con-vs-loading');
+        },2000);
+      }
+
+    }
+  }
+</script>
+
+<style lang="stylus">
+</style>
