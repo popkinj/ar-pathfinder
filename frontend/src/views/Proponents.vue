5ef88e32bdc9605937a8e1836fcbcbe4fdb7dd9c
Jamie Popkin <popkinj@littleearth.ca>
2019-02-16 17:00:10 -0800

    text entry dummy search tool

--- frontend/src/views/Proponents.vue
+++ frontend/src/views/Proponents.vue
@@ -12,10 +12,34 @@
             ref="loadableButton"
             vslor="primary")
               | Load
+        th
+      tr
+        th Search Proponents
+        th
+          vs-input(
+            class="inputx"
+            label-placeholder="Name"
+            v-model="value1"
+          )
+        th
+          vs-button(
+            id="button-with-loading2"
+            class="vs-con-loading__container"
+            @click="openLoadingContained2"
+            type="filled"
+            ref="loadableButton2"
+            vslor="primary")
+              | Search
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        value1:'',
+        value2:''
+      }
+    },
     methods: {
       openLoadingContained(){
         this.$vs.loading({
@@ -27,6 +51,17 @@
         setTimeout( () => {
           this.$vs.loading.close('#button-with-loading .con-vs-loading');
         },2000);
+      },
+      openLoadingContained2(){
+        this.$vs.loading({
+          background: this.backgroundLoading,
+          color: this.colorLoading,
+          container: '#button-with-loading2',
+          scale: 0.45
+        });
+        setTimeout( () => {
+          this.$vs.loading.close('#button-with-loading2 .con-vs-loading');
+        },2000);
       }
 
     }
@@ -34,4 +69,11 @@
 </script>
 
 <style lang="stylus">
+  th
+    padding-top 2rem
+  .vs-con-input-label
+    margin-top 0.25rem !important
+  .default-input
+    .inputx
+      margin 10px
 </style>
