189ae1f092efe2b0396f98611a23b274f34fcb0f
Jamie Popkin <popkinj@littleearth.ca>
2019-02-19 17:16:05 -0800

    A better title

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Dumping ground for API Testing"/>
+    <HelloWorld msg="API Testing Playground for Iterative Development"/>
   </div>
 </template>
 
