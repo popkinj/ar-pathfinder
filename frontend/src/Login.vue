480af1492fc1b30bd55d398c6cdb0e455917622d
Jamie Popkin <popkinj@littleearth.ca>
2019-03-13 16:25:10 -0700

    This may bring vuesax in... Not sure

--- frontend/src/Login.vue
+++ frontend/src/Login.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
-  .page Login Page
-    //- keep-alive
-    //-   transition(name="fade", mode="out-in")
+.page Login Page
+  //- keep-alive
+  //-   transition(name="fade", mode="out-in")
 </template>
 
 <script>
+require('vuesax');
 </script>
