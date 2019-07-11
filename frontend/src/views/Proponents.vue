07ab917e537e487bd3442317b32df150b3c88ce9
Jamie Popkin <popkinj@littleearth.ca>
2019-07-11 16:42:43 -0700

    Simple formatting

--- frontend/src/views/Proponents.vue
+++ frontend/src/views/Proponents.vue
@@ -2,40 +2,51 @@
   div.proponents
     h2 Proponents
     GetToken
-    .tester
-      .name Fetch all Proponents
-      .action
-        vs-button(
-          :disabled="$store.getters.token ? false : true"
-          id="button-with-loading"
-          class="vs-con-loading__container"
-          @click="getAllProponents"
-          type="filled"
-          ref="loadableButton"
-          vslor="primary")
-            | Load
-    .tester
-      .name Search Proponents
-      .action
-        vs-input(
-          :disabled="$store.getters.token ? false : true"
-          class="inputx"
-          label-placeholder="Enter a Name"
-          @input="updateSearch"
-          v-model="value1"
-        )
-      .action
-        vs-button(
-          :disabled="$store.getters.token ? false : true"
-          id="button-with-loading2"
-          class="vs-con-loading__container"
-          @click="openLoadingContained2"
-          type="filled"
-          ref="loadableButton2"
-          vslor="primary")
-            | Search
+    table
+      tr.tester
+        td.action
+          vs-button(
+            :disabled="$store.getters.token ? false : true"
+            id="button-with-loading"
+            class="vs-con-loading__container"
+            @click="getAllProponents"
+            type="filled"
+            ref="loadableButton"
+            vslor="primary")
+              | Load
+        td.name Fetch all Proponents
+      tr.tester
+        td.action
+          vs-button(
+            :disabled="$store.getters.token ? false : true"
+            id="button-with-loading2"
+            class="vs-con-loading__container"
+            @click="openLoadingContained2"
+            type="filled"
+            ref="loadableButton2"
+            vslor="primary")
+              | Search
+        td.action
+          vs-input(
+            :disabled="$store.getters.token ? false : true"
+            class="inputx"
+            label-placeholder="Enter a Name"
+            @input="updateSearch"
+          )
+        td.name Search Proponents
     #proponents
-      div(v-for="item in $store.getters.proponents.items") {{item.customer_name}}
+      vs-table
+        template(slot="thead")
+          vs-th Name
+          vs-th Proponent Number
+          vs-th Business Number
+        template
+          vs-tr(:key="indextr" v-for="(tr,indextr) in $store.getters.proponents.items")
+            vs-td {{tr.customer_name}}
+            vs-td {{tr.party_number}}
+            vs-td {{tr.business_number}}
+
+      //- div(v-for="item in $store.getters.proponents.items") {{item.customer_name}}
 </template>
 
 <script>
@@ -45,8 +56,6 @@
   export default {
     data() {
       return {
-        value1:'',
-        value2:''
       }
     },
     components: {
@@ -97,6 +106,12 @@
 </script>
 
 <style lang="stylus">
+  td
+    padding 0.5rem
+    text-align left
+
+    button 
+      width 5rem
   .vs-con-input-label
     margin-top 0.25rem !important
   .default-input
@@ -107,4 +122,11 @@
     position absolute
     right 2rem
     top 2rem
+
+  #proponents
+    text-align left
+    margin-top 2rem
+
+  .not-data-table
+    display none
 </style>
